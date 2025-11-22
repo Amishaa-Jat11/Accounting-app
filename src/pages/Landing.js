b36881f018aed6c52fc61e2f163580d72b6e79dc
Amishaa-Jat11 <amishajat15@gmail.com>
2025-11-22 18:33:41 +0530

    change in navbar

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -66,7 +66,7 @@ const Landing = () => {
               </Link>
               <Link 
                 to="/signup"
-                className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors font-medium"
+                className="bg-white text-green-600 px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors font-medium"
               >
                 Sign Up
               </Link>
